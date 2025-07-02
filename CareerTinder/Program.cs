a360d669f13b4609a2eb08294453364658743690
lironsh11 <lironsh11@gmail.com>
2025-07-02 20:40:29 +0300

    DA

--- CareerTinder/Program.cs
+++ CareerTinder/Program.cs
@@ -2,13 +2,30 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// -------------------------------------------
+// Listen on specific port (5002) on all interfaces
+// -------------------------------------------
+if (builder.Environment.IsDevelopment())
+{
+    // Local debugging - bind to localhost only
+    builder.WebHost.UseUrls("https://localhost:7055/");
+}
+else
+{
+    // Production/public - bind to all interfaces
+    builder.WebHost.UseUrls("http://0.0.0.0:5002");
+}
+
+// -------------------------------------------
 // Add services to the container
-builder.Services.AddControllers();
+// -------------------------------------------
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// -------------------------------------------
 // Configure the HTTP request pipeline
+// -------------------------------------------
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -17,8 +34,14 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
 
+app.UseAuthorization();
+
+// -------------------------------------------
+// Configure routing
+// -------------------------------------------
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
